30432e0616295ab196a022ab10e39c5a01fff34b
Anudit Verma <verma.anudit@gmail.com>
2015-08-04 03:07:52 +0530

    Updated index.php
    -Added pledge id
    -Fixed contact_id key values

--- api/v3/People/index.php
+++ api/v3/People/index.php
@@ -7,43 +7,46 @@ $json = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/e
 
 $json2 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=DashboardContact&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
 
+$json3 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=Pledge&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+
 
 
 $array = json_decode($json, true);
 $array2 = json_decode($json2, true);
+$array3 = json_decode($json3, true);
 
 $hal = new \Nocarrier\Hal('/sites/default/ext/org.civicrm.osdi/api/v3/People/', ['per_page' => 236,'page' => 1,'total_records' => 236]);
 
 foreach($array['values'] as $key => $value){
     $i = $array['values'][$key]['contact_id'];
 $resource = new \Nocarrier\Hal(
-    '/People?contact_id='.$array['values'][$i]['contact_id'],
+    '/People?contact_id='.$array['values'][$key]['contact_id'],
     array(
-        'given_name' => $array['values'][$i]['given_name'],
-        'family_name' => $array['values'][$i]['family_name'],
+        'given_name' => $array['values'][$key]['given_name'],
+        'family_name' => $array['values'][$key]['family_name'],
         'email_addresses' => array(
             'primary' => true,
-            'address' => $array['values'][$i]['email']),
-        'identifiers' => array('osdi-person-'.'['.$i.']'),
-        'id'=> $array['values'][$i]['contact_id'],
+            'address' => $array['values'][$key]['email']),
+        'identifiers' => array('osdi-person-'.'['.$key.']'),
+        'id'=> $array['values'][$key]['contact_id'],
         'created_date' => $array2['values'][$i]['created_date'],
         'modified_date' => date("Y/m/d"),
         'custom_fields' => array(
-            'email' => $array['values'][$i]['email'],
-            'full_name' => $array['values'][$i]['given_name'].' '.$array['values'][$i]['family_name'],
+            'email' => $array['values'][$key]['email'],
+            'full_name' => $array['values'][$key]['given_name'].' '.$array['values'][$key]['family_name'],
             'event_code' => 'xx',
-            'address' => $array['values'][$i]['postal_addresses'],
-            'zip' => $array['values'][$i]['zip_code'],
-            'pledge' => 'num'),
+            'address' => $array['values'][$key]['postal_addresses'],
+            'zip' => $array['values'][$key]['zip_code'],
+            'pledge' => $array3['values'][$i]['pledge_id']),
         'postal_addresses' => array(
             array(
             'address_lines' => array(null),
-            'postal_code' => $array['values'][$i]['zip_code'],
+            'postal_code' => $array['values'][$key]['zip_code'],
             'address_status' => 'Verified/Not Verified',
             'primary' => 'True/False',)),
         'phone_numbers' => array(
             array(
-            'number' => $array['values'][$i]['number'],)),
+            'number' => $array['values'][$key]['number'],)),
         '_embedded' => array(
             'osdi:tags' => array())
         )
