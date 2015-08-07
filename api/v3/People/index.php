576233bb96ffead0c5139137670156ce860893a2
Anudit Verma <verma.anudit@gmail.com>
2015-08-08 01:11:35 +0530

    Updated index.php
    Added automated counter for showing number for records.

--- api/v3/People/index.php
+++ api/v3/People/index.php
@@ -9,13 +9,13 @@ $json2 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/
 
 $json3 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=Pledge&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
 
-
-
 $array = json_decode($json, true);
 $array2 = json_decode($json2, true);
 $array3 = json_decode($json3, true);
 
-$hal = new \Nocarrier\Hal('/sites/default/ext/org.civicrm.osdi/api/v3/People/', ['per_page' => 236,'page' => 1,'total_records' => 236]);
+$count=sizeof($array['values']);
+
+$hal = new \Nocarrier\Hal('/sites/default/ext/org.civicrm.osdi/api/v3/People/', ['per_page' => $count,'page' => 1,'total_records' => $count]);
 
 foreach($array['values'] as $key => $value){
     $i = $array['values'][$key]['contact_id'];
