2d1ea1ef7682a1cbf03dc7ace87fe97561f015b2
Anudit Verma <verma.anudit@gmail.com>
2015-08-17 17:43:43 +0530

    Updated test.php

--- api/v3/People/test.php
+++ api/v3/People/test.php
@@ -1,11 +1,14 @@
 <?php
 
-$json = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={"sequential":1}&options[limit]=0&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+$json = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={"sequential":1}&options[limit]=0&&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+
+$json2 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=DashboardContact&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+
+$json3 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=Pledge&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
 
 $array = json_decode($json, true);
-
-$ff = array_filter_recursive($array);
-echo json_encode($ff);
+$array2 = json_decode($json2, true);
+$array3 = json_decode($json3, true);
 
 function array_filter_recursive($input, $callback = null) {
     if (!is_array($input)) {
@@ -18,10 +21,6 @@ function array_filter_recursive($input, $callback = null) {
     return array_filter($input, $callback);
 }
 
-
-$result = array_walk_recursive_delete($array, function ($value, $key) {
-     if (is_array($value)) {
-            return empty($value);
-         }
-            return ($value === null);
-        });
+$array = array_filter_recursive($array);
+$array2 = array_filter_recursive($array2);
+$array3 = array_filter_recursive($array3);
