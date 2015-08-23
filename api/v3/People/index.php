a57c46e03a7b02bd833abe2ec699ddf1f0740137
Anudit Verma <verma.anudit@gmail.com>
2015-08-23 13:03:02 +0530

    Fixed issue/6
    Added more address fields.

--- api/v3/People/index.php
+++ api/v3/People/index.php
@@ -13,9 +13,9 @@ $count=sizeof($array['values']);
 $hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/index.php', ['per_page' => $count,'page' => 1,'total_records' => $count]);
 
 foreach($array['values'] as $key => $value){
-    $i = $array['values'][$key]['contact_id'];
+    $i = $array['values'][$key]['id'];
  $resource = new \Nocarrier\Hal(
-    '/People?contact_id='.$array['values'][$key]['contact_id'],
+    '/People?contact_id='.$array['values'][$key]['id'],
     array(
         'given_name' => $array['values'][$key]['given_name'],
         'family_name' => $array['values'][$key]['family_name'],
@@ -24,18 +24,20 @@ foreach($array['values'] as $key => $value){
                 'primary' => true,
                 'address' => $array['values'][$key]['email'])),
         'identifiers' => array('civi_crm:'.$i),
-        'id'=> $array['values'][$key]['contact_id'],
+        'id'=> $array['values'][$key]['id'],
         'created_date' => date("c", strtotime($array2['values'][$i]['created_date'])),
-        'modified_date' => date("c", strtotime($array2['values'][$i]['modified_date'])),
+        'modified_date' => date("c", strtotime($array['values'][$i]['modified_date'])),
         'custom_fields' => array(),
         'postal_addresses' => array(
             array(
             'address_lines' => array(
                 array($array['values'][$key]['postal_addresses']),
                 ),
-            'postal_code' => $array['values'][$key]['zip_code'],
-            'address_status' => 'Verified/Not Verified',
-            'primary' => 'True/False',)),
+            'locality' => $array['values'][$key]['city'],
+            'region' => $array['values'][$key]['state_province_name'],
+            'postal_code' => $array['values'][$key]['postal_code'],
+            'country'=> $array['values'][$key]['country'],
+            'primary' => filter_var($array3['values'][$key]['is_primary'], FILTER_VALIDATE_BOOLEAN))),
         'phone_numbers' => array(
             array(
             'number' => $array['values'][$key]['number'],))
