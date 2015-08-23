ca10147a11fd6aed1659d14ac00be931ae29e969
Anudit Verma <verma.anudit@gmail.com>
2015-08-24 01:33:13 +0530

    Fixed issue/15

--- api/v3/People/person.php
+++ api/v3/People/person.php
@@ -2,26 +2,30 @@
 
 header("Content-Type:application/hal+json");
 
-include 'blank.php';
-
 include 'country_codes.php';
 
 require_once '/srv/www/buildkit/build/drupal-demo/sites/all/libraries/vendor/autoload.php';
 
 use Nocarrier\Hal;
 
-$key = $_GET['id'];
-$key--;
-$i = $key;
+$id = $_GET['id'];
 
-$hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/person.php'.'?id='.$i,
+$json = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={"sequential":1,"id":'.$id.'}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+
+$json3 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=Address&action=get&json={"sequential":1,"id":'.$id.'}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+
+$array = json_decode($json, true);
+$array3 = json_decode($json3, true);
+
+foreach($array['values'] as $key => $value){
+$hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/person.php'.'?id='.$id,
    ['given_name' => $array['values'][$key]['given_name'],
     'family_name' => $array['values'][$key]['family_name'],
         'email_addresses' => array(
             array(
                 'primary' => true,
                 'address' => $array['values'][$key]['email'])),
-    'identifiers' => array('civi_crm:'.$i),
+    'identifiers' => array('civi_crm:'.$id),
     'id'=> $array['values'][$key]['contact_id'],
     'created_date' => date("c", strtotime($array2['values'][$i]['created_date'])),
     'modified_date' => date("c", strtotime($array2['values'][$i]['modified_date'])),
@@ -40,5 +44,6 @@ $hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/person.php'.'?i
         array(
         'number' => $array['values'][$key]['number'],))]
  );
+}
 
 echo $hal->asJson();
