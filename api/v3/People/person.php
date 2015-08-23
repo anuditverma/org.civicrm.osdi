ece496ae627149314cf3ab24a1cd25ae607e9ad7
Anudit Verma <verma.anudit@gmail.com>
2015-08-23 23:09:12 +0530

    Fixed issue/6
    For converting country code into 2 character ISO code.

--- api/v3/People/person.php
+++ api/v3/People/person.php
@@ -4,13 +4,15 @@ header("Content-Type:application/hal+json");
 
 include 'blank.php';
 
+include 'country_codes.php';
+
 require_once '/srv/www/buildkit/build/drupal-demo/sites/all/libraries/vendor/autoload.php';
 
 use Nocarrier\Hal;
 
 $key = $_GET['id'];
 $key--;
-$i = $array['values'][$key]['contact_id'];
+$i = $key;
 
 $hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/person.php'.'?id='.$i,
    ['given_name' => $array['values'][$key]['given_name'],
@@ -25,13 +27,15 @@ $hal = new \Nocarrier\Hal('/sites/default/ext/osdi/api/v3/People/person.php'.'?i
     'modified_date' => date("c", strtotime($array2['values'][$i]['modified_date'])),
     'custom_fields' => array(),
     'postal_addresses' => array(
-            array(
+         array(
             'address_lines' => array(
                 array($array['values'][$key]['postal_addresses']),
                 ),
-    'postal_code' => $array['values'][$key]['zip_code'],
-    'address_status' => 'Verified/Not Verified',
-    'primary' => 'True/False',)),
+            'locality' => $array['values'][$key]['city'],
+            'region' => $array['values'][$key]['state_province_name'],
+            'postal_code' => $array['values'][$key]['postal_code'],
+            'country'=> array_search($array['values'][$key]['country'], $countrycodes),
+            'primary' => filter_var($array3['values'][$key]['is_primary'], FILTER_VALIDATE_BOOLEAN))),
     'phone_numbers' => array(
         array(
         'number' => $array['values'][$key]['number'],))]
