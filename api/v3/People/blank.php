9535bfdd9fea51f4a611c0cd081b9dbd00d15a27
Anudit Verma <verma.anudit@gmail.com>
2015-08-27 13:22:19 +0530

    Update blank.php
    Generalizing the code.

--- api/v3/People/blank.php
+++ api/v3/People/blank.php
@@ -1,10 +1,10 @@
 <?php
 
-$json = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={"sequential":1}&options[limit]=0&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+$json = file_get_contents('<your CiviCRM site>/sites/all/modules/civicrm/extern/rest.php?entity=People&action=get&json={"sequential":1}&options[limit]=0&api_key=yourkey&key=sitekey');
 
-$json2 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=DashboardContact&action=get&json={"sequential":1}&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+$json2 = file_get_contents('<your CiviCRM site>/sites/all/modules/civicrm/extern/rest.php?entity=DashboardContact&action=get&json={"sequential":1}&api_key=yourkey&key=sitekey');
 
-$json3 = file_get_contents('http://camus.fuzion.co.nz/sites/all/modules/civicrm/extern/rest.php?entity=Address&action=get&json={"sequential":1}&options[limit]=0&api_key=9BivcYv1cOT7md6Rxom8Stiz&key=gNhqb5uGUaiLAHrZ');
+$json3 = file_get_contents('<your CiviCRM site>/sites/all/modules/civicrm/extern/rest.php?entity=Address&action=get&json={"sequential":1}&options[limit]=0&&api_key=yourkey&key=sitekey');
 
 $array = json_decode($json, true);
 $array2 = json_decode($json2, true);
